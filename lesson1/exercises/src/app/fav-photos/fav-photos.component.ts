a548691e63e9faf2d3b15a1e03ba24c7f866fbbf
BeltedKingfisher <AmyJSeibert@gmail.com>
2021-06-25 09:56:58 -0500

    finished exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Here are some pictures';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://media4.giphy.com/media/fpO7QkiPMFKSmdupRD/giphy.gif?cid=ecf05e477caot031i7iu4y9g0nt0cw8u5day2t5vonzfhdyc&rid=giphy.gif&ct=g';
   image3 = '';
 
   constructor() { }
